35c4f8187f22a600cdf2c396d1863aae139a2fc5
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-17 01:59:00 -0400

    h1 and h3 on the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,10 @@ class Home extends Component {
     render() {
         return (<div>
             <img className="koifish__picture" src={Aquarium} alt="" width="1440px"></img>
-            <h1 className="koifish__text"> Welcome to Striking Fish Photos! </h1>
-            <h2 className="koifish__text"> Here at Striking Fish Photos, we are dedicated to showing the internet the beauty of under water aquatics, and the tranquility of aquamarine life</h2>
+            <div className="koifish__text">
+            <h1> Welcome to Striking Fish Photos! </h1>
+            <h2> Here at Striking Fish Photos, we are dedicated to showing the internet the beauty of under water aquatics, and the tranquility of aquamarine life</h2>
+            </div>
         </div>);
     }
 }
