ecacc501cbd828a7a6aff49ff74eab21210b718f
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-17 11:45:19 -0400

    adding comments to navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import About from './components/About'
-import Pictures from './components/Pictures'
-import Settings from './components/Settings'
-import Favorites from './components/Favorites'
-import Aquarium from '../assets/koifish.jpg'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import About from './components/About';
+import Pictures from './components/Pictures';
+import Settings from './components/Settings';
+import Favorites from './components/Favorites';
+import Aquarium from '../src/assets/koifish.jpg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
 
@@ -17,10 +17,13 @@ class App extends React.Component {
         <div className="App">
           <header className="App-header">
             <Navbar />
-            <Route path='/about' component={About} />
-            <Route path='/pictures' component={Pictures} />
-            <Route path='/favorites' component={Favorites} />
-            <Route path='/settings' component={Settings} />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/pictures' component={Pictures} />
+              <Route path='/favorites' component={Favorites} />
+              <Route path='/settings' component={Settings} />
+            </Switch>
           </header>
         </div>
       </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+// Create a Navbar component
+
 import React, { Component } from 'react';
 
 class Navbar extends Component {
@@ -6,6 +8,7 @@ class Navbar extends Component {
         return (<div>
             <div className="navbar-container">
                 <ul class="navbar">
+                 {/* list items used as links to different pages */}
                     <li className="navbar__list">Home</li>
                     <li className="navbar__list">About</li>
                     <li className="navbar__list">Pictures</li>
