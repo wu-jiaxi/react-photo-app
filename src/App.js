e78f3244dc9c306aa746b623cabc655ca6144801
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-17 11:16:20 -0400

    adding route for homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Home from './components/Home'
 import About from './components/About'
 import Pictures from './components/Pictures'
 import Settings from './components/Settings'
 import Favorites from './components/Favorites'
+import Aquarium from '../assets/koifish.jpg'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 class App extends React.Component {
@@ -14,20 +14,30 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <header className="App-header">
-          <Navbar />
-          <Route path='/home' component={Home} />
-          <Route path='/about' component={About} />
-          <About />
-          <Pictures />
-          <Favorites />
-          <Settings />
-        </header>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <Navbar />
+            <Route path='/about' component={About} />
+            <Route path='/pictures' component={Pictures} />
+            <Route path='/favorites' component={Favorites} />
+            <Route path='/settings' component={Settings} />
+          </header>
+        </div>
       </Router>
     );
   }
 }
+
+const Home = () => (
+  <div>
+    <img className="koifish__picture" src={Aquarium} alt=""></img>
+    <div className="koifish__text">
+      <h1> Welcome to Striking Fish Photos! </h1>
+      <h2> Here at Striking Fish Photos, we are dedicated </h2>
+      <h2> to showing the beauty of underwater aquatics, </h2>
+      <h2>and the tranquility of aquamarine life</h2>
+    </div>
+  </div>
+);
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import Aquarium from '../assets/koifish.jpg'
 
 class Home extends Component {
     state = {}
     render() {
         return (<div>
-            <img className="koifish__picture" src={Aquarium} alt=""></img>
-            <div className="koifish__text">
-            <h1> Welcome to Striking Fish Photos! </h1>
-            <h2> Here at Striking Fish Photos, we are dedicated </h2>
-            <h2> to showing the beauty of underwater aquatics, </h2>
-            <h2>and the tranquility of aquamarine life</h2>
-            </div>
+            
         </div>);
     }
 }
