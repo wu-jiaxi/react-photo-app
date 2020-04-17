aca61eaf4ada5fa7786462294b6ed16688ce11ea
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-17 02:16:40 -0400

    setting.js

---
+++ src/components/Settings.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Setting extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+            
+        </div> );
+    }
+}
+ 
+export default Setting;
