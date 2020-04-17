00fa2de5bc4c32f0222a479020219fae47a54273
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-17 02:14:14 -0400

    favorite.js

---
+++ src/components/Favorites.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+            
+        </div> );
+    }
+}
+ 
+export default Favorites;
