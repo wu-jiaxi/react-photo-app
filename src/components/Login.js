6b1d9bab18a24cc459a49f749fe0d8fece32045b
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-17 16:09:02 -0400

    added sign in form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import Aquarium from '../assets/koifish.jpg'
+import Aquarium from '../assets/artkoi2.png'
 
 class Login extends Component {
     state = {}
     render() {
         return (<div>
             <img className="koifish__picture" src={Aquarium} alt=""></img>
+            <form className="login-form">
+                Sign In
+                <label className="login__input-field">
+                    Email:
+                   <input type='text' name='name' />
+                </label>
+                <label className="login__input-field">
+                    Password
+                   <input type='text' name='name' />
+                </label>
+                New here?
+                <input type="submit" value="Submit" />
+            </form>
         </div>);
     }
 }
