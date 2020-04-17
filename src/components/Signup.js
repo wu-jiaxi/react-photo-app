7e7353dea74598b1ed40665c680ffb778f089874
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-17 19:53:33 -0400

    added picture to signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Aquarium from '../assets/artkoi2.png'
 
 class Signup extends Component {
     state = {}
     render() {
         return (<div>
-            <h1> Signup </h1>
+             <img className="koifish__picture" src={Aquarium} alt=""></img>
         </div>);
     }
 }
