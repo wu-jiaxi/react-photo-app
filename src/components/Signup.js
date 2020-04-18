3f0cdcd5f2d4a44ca65288e35d5f59b067d8476b
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-17 21:00:02 -0400

    adding firebase

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,6 +6,22 @@ class Signup extends Component {
     render() {
         return (<div>
              <img className="koifish__picture" src={Aquarium} alt=""></img>
+             <form className="login-form">
+                Sign Up
+                <label className="login__input-field">
+                    Email:
+                   <input type='text' name='name' />
+                </label>
+                <label className="login__input-field">
+                    Password
+                   <input type='text' name='name' />
+                </label>
+                <label className="login__input-field">
+                    Date of Birth
+                   <input type='text' name='name' />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </div>);
     }
 }
