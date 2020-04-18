3245db6d3814247c48850fdf7dc79b9ccc581edc
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-18 03:37:30 -0400

    function handleChange and handleSubmit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,26 +6,35 @@ class Signup extends Component {
     state = {
 
     }
+
+    handleChange = (e) =>{
+        console.log(e);
+    }
+
+    handleSubmit = (e) => {
+        console.log(e);
+    }
+
     render() {
         return (<div>
             <img className="koifish__picture" src={Aquarium} alt=""></img>
             {/* making container box */}
             <div className="container"> 
-                <form onSubmit={} className="login-form">
+                <form className="login-form">
                     Sign Up
                 <label className="login__input-field">
                         Email:
-                   <input type='text' name='email' placeholder="email" onChange={} />
+                   <input type='text' name='email' placeholder="email" onChange={this.handleChange} />
                     </label>
                     <label className="login__input-field">
                         Password
-                   <input type='text' name='password' placeholder="password" />
+                   <input type='text' name='password' placeholder="password" onChange={this.handleChange} />
                     </label>
                     <label className="login__input-field">
                         Date of Birth
-                   <input type='text' name='birth' placeholder="birth" />
+                   <input type='text' name='birth' placeholder="birth" onChange={this.handleChange} />
                     </label>
-                    <input type="submit" value="Submit" />
+                    <input type="submit" value="Submit" onSubmit={this.handleSubmit}/>
                 </form>
             </div>
         </div>);
