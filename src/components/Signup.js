fdfbb250060df983e163dd49d8b296d32702ab00
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-18 00:18:59 -0400

    signup.js container

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
 import Aquarium from '../assets/artkoi2.png'
 
+
 class Signup extends Component {
-    state = {}
+    state = {
+
+    }
     render() {
         return (<div>
-             <img className="koifish__picture" src={Aquarium} alt=""></img>
-             <form className="login-form">
-                Sign Up
+            <img className="koifish__picture" src={Aquarium} alt=""></img>
+            {/* making container box */}
+            <div className="container"> 
+                <form onSubmit={} className="login-form">
+                    Sign Up
                 <label className="login__input-field">
-                    Email:
-                   <input type='text' name='name' />
-                </label>
-                <label className="login__input-field">
-                    Password
-                   <input type='text' name='name' />
-                </label>
-                <label className="login__input-field">
-                    Date of Birth
-                   <input type='text' name='name' />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+                        Email:
+                   <input type='text' name='email' placeholder="email" />
+                    </label>
+                    <label className="login__input-field">
+                        Password
+                   <input type='text' name='password' placeholder="password" />
+                    </label>
+                    <label className="login__input-field">
+                        Date of Birth
+                   <input type='text' name='birth' placeholder="birth" />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         </div>);
     }
 }
 
 export default Signup;
+
