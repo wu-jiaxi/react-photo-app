f76ce3c70f3985cc226d7b248a0f4348a5847864
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-04-18 14:03:50 -0400

    adding handleChange and handleSubmit functions for Login.js componenent

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,19 @@ import Aquarium from '../assets/artkoi2.png'
 import { Link } from 'react-router-dom'
 
 class Login extends Component {
-    state = {}
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleChange = (e) =>{
+        console.log(e);
+    }
+
+    handleSubmit = (e) => {
+        console.log(e);
+    }
+
     render() {
         return (<div>
             <img className="koifish__picture" src={Aquarium} alt=""></img>
@@ -11,11 +23,11 @@ class Login extends Component {
                 Sign In
                 <label className="login__input-field">
                     Email:
-                   <input type='text' name='name' />
+                   <input type='text' name='name' onChange={this.handleChange}/>
                 </label>
                 <label className="login__input-field">
                     Password
-                   <input type='text' name='name' />
+                   <input type='text' name='password' onChange={this.handleChange}/>
                 </label>
                 <Link to='/signup'>
                     New here?
